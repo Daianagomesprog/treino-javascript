39452e949e9e498038b530ca1475ae52eefc15e1
Daiana <daiana.gomes24@gmail.com>
2021-07-28 23:07:05 -0300

    Outro commit
    Apenas para aparecer lá!

--- exercicio-teste.js
+++ exercicio-teste.js
@@ -2,7 +2,7 @@ var numero = 20;
 var outroNumero = 30;
 var soma = numero + outroNumero;
 if (soma == 50){
-    console.log("É igual a 50!"); 
+    console.log("É igual a xpto!"); 
 
 } else {
    console.log("Não é igual a xpto."); 
